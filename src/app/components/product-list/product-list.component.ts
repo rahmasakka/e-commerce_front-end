03eabedd0e8b99ac203bfa4a4f646530a3bafa51
rahmasakka <rahmasakka982@gmail.com>
2022-02-25 10:49:53 +0100

    Detail View Product

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -44,7 +44,6 @@ export class ProductListComponent implements OnInit {
         this.products = data;
       }
     )
-
   }
 
 
@@ -54,7 +53,6 @@ export class ProductListComponent implements OnInit {
 
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     // const hasCategoryId: boolean = this.route/*use the activated route*/.snapshot/*State of route at this given moment in time*/.paramMap/*Map of all the route parameters*/.has('id');
-
 
     if (hasCategoryId) {
       // get the "id" param string. convert string to a numberusing the "+" symbol
@@ -66,7 +64,6 @@ export class ProductListComponent implements OnInit {
 
     console.log(this.productService.getProductList(this.currentCategoryId));
 
-
     //now get the products for the given category id 
     this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
@@ -74,5 +71,4 @@ export class ProductListComponent implements OnInit {
       }
     )
   }
-
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -14,10 +14,7 @@ export class ProductService {
   private baseUrl = "http://localhost:8088/api/products";
   private categoryUrl = "http://localhost:8088/api/product-category";
 
-
-
   constructor(private httpClient: HttpClient) { }
-
 
   getProductList(theCategoryId: number): Observable<Product[]> {
 
@@ -36,6 +33,12 @@ export class ProductService {
     return this.httpClient.get<GetResponseProducts>(searchUrl).pipe(map(response => response._embedded.products))
   }
 
+  getProduct(theProductId: number): Observable<Product> {
+    //need to build URL base on product id
+    const productUrl = `${this.baseUrl}/${theProductId}`;
+    return this.httpClient.get<Product>(productUrl);
+  }
+
 
   getProductCategories(): Observable<ProductCategory[]> {
     return this.httpClient.get<GetResponseProductCategory>(this.categoryUrl).pipe(
@@ -43,11 +46,7 @@ export class ProductService {
       map(response => response._embedded.productCategory)
     )
   }
-
 }
-
-
-
 
 interface GetResponseProducts {
   _embedded: {
